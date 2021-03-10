4bc3f01ea5e61629050a382c0200c6517ed7115a
Bob The Builder <bob@the.builder>
2021-03-10 22:49:28 +0530

    First Commit

---
+++ Assets/CubeScalingSouth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScalingSouth : MonoBehaviour
+{
+    float i = 1;
+    // Start is called before the first frame update
+    public void scaling_cube()
+    {
+        i = i + 0.5f;
+        transform.localScale = new Vector3(1f, 1f, i);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Color myColor = GetComponent<Renderer>().material.color;
+        Color otherColor = collision.gameObject.GetComponent<Renderer>().material.color;
+        if (myColor.Equals(otherColor) || collision.gameObject.CompareTag("center_cube"))
+        {
+            i = 1;
+            transform.localScale = new Vector3(1, 1, 1);
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/RandomSpawnScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawnScript : MonoBehaviour
+{
+    public GameObject prefab1, prefab2, prefab3, prefab4;
+
+    public float spawnRate = 3f;
+
+    float nextSpawn = 0f;
+
+    int whatToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            //Destroy(gameObject);
+            whatToSpawn = Random.Range(1, 5);
+            Debug.Log(whatToSpawn);
+            switch (whatToSpawn)
+            {
+                case 1:
+                    Instantiate(prefab1, transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(prefab2, transform.position, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(prefab3, transform.position, Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(prefab4, transform.position, Quaternion.identity);
+                    break;
+            }
+
+            nextSpawn = Time.time + spawnRate;
+        }
+        
+    }
+
+}
